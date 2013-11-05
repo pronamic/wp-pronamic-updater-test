a9132923b3e94fe468d4aefe00a0420dbc8fbb74
Remco Tolsma <info@remcotolsma.nl>
2013-11-05 12:04:43 +0100

    Init release.

---
+++ pronamic-updater-test.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Pronamic Updater Test
+Plugin URI: http://www.pronamic.eu/
+Description: 
+Version: 1.0.0
+Author: Pronamic
+Author URI: http://www.pronamic.eu/
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+Network: true
+*/
+
